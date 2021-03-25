41b691b3d894896408431baa9c696e16838e1cd3
Statsgeek <statsgeeknz@googlemail.com>
2021-03-25 15:30:43 +0000

    Update National Summary.r
    Creates good results for the "percentage" type question estimates - deff estimates are still out.

--- R conversion/National Summary.r
+++ R conversion/National Summary.r
@@ -13,24 +13,29 @@
   library(tidyverse)
   library(srvyr)
 
+  #= only plays a role in the names of files - redundant in the SAS example
+  Year <- 1920
+
+  #= paths
+  dataPath <- "data/"
+  
   #= read in example datasets - data management in reality TBD
   HACE1920_QUESTIONS <- read_csv("data/HACE1920_QUESTIONS.csv")
   HACE1920_STRATA_DATA <- read_csv("data/HACE1920_STRATA_DATA - Dummy data.csv")
   HACE1920_WEIGHTED <- read_csv("data/HACE1920_WEIGHTED - Dummy data.csv")
 
-
-  Year <- 1920
+  #= range of questions to ingest (indexed by variable iref)
   first_question <- 101
   last_question <- 153
+
+  #= gives a suffix for a variable called from the data
   geography <- "NAT"
-
-
 
 # Pre-macro data manipulation -------------------------------------------------------------------------------------------------------------------
 
 
   # HSC comment: "Set up strata populations"
-  Strata_Pop <- HACE1920_STRATA_DATA %>% rename(total = n_eligible) %>% select(strata, total) %>% arrange(strata)
+  Strata_Pop <- HACE1920_STRATA_DATA %>% rename(total = n_eligible) %>% select(strata, total) %>% arrange(strata) %>% rename(Strata = strata)
 
 
   # HSC comment: "Calculate the iref for the first percent positive question in the questions of interest:"
@@ -77,35 +82,47 @@
     
   #= Code whether positive (1/2), or pos/neut/neg (1/2/3) depending on definition 
    
-    workingWeights <- workingWeights %>%
-      rowwise() %>%
+  
+    workingWeights <- workingWeights %>% rowwise() %>%  
       mutate(
-            PercentPositive = ifelse(grepl(Question, Positive), '% Positive', '% Not Positive'),
-             PosNeutNeg = case_when(
-               grepl(Question, Positive) ~ '% Positive',
-               grepl(Question, Neutral) ~ '% Neutral',
-               grepl(Question, Negative) ~ '% Negative'
-             )
+        PercentPositive = ifelse(grepl(Question, Positive), '% Positive', '% Not Positive'),
+        PosNeutNeg = ifelse(grepl(Question, Positive), '% Positive', ""), 
+        PosNeutNeg = ifelse(grepl(Question, Neutral), '% Neutral', PosNeutNeg), 
+        PosNeutNeg = ifelse(grepl(Question, Negative), '% Negative', PosNeutNeg)
       )
     
-    
+
   #= Generate the equivalent of the SAS one-way table, as defined:
-    #     TITLE "Results: &Question";
-    #     PROC SURVEYFREQ DATA=Responses_&Question TOTAL=Strata_Pop NOSUMMARY;
-    #     TABLES &Question PosNeutNeg_&Question PercentPositive_&Question / cl row(deff) deff;
-    #     STRATA Strata; WEIGHT &Weight;
+
+    workingSurvey <- workingWeights %>% right_join(Strata_Pop, by = "Strata")
     
-    test <- as_survey(workingWeights, strata = Strata, weight = Weight)
-    test %>% group_by(Question) %>% summarise(pct = survey_prop(deff = "replace"))
+    workingSurvey <- as_survey(workingSurvey, strata = Strata, weight = Weight, fpc = total) 
+    
+    workingSurvey %>% group_by(Question) %>% summarise(Freq = n(), WeightFreq = survey_total(), pct = survey_prop(vartype = c("se", "ci"), deff = T))
+    workingSurvey %>% group_by(PercentPositive) %>% summarise(pct = survey_prop(deff = "replace"))
+    workingSurvey %>% group_by(PosNeutNeg) %>% summarise(pct = survey_prop(deff = "replace"))
    
     
     
 # Non  Indicator OR Information questions -------------------------------------------------------------------------------------------------------
 
  
-      
+    currentQuestion <- indicatorQuestions[[1]]
  
-      
+
+    
+    workingWeights <- HACE1920_WEIGHTED %>% 
+      select(GP_PRAC_NO, n_eligible, currentQuestion$Question, currentQuestion$Weight2) %>%
+      mutate(Exclude = currentQuestion$Exclude) %>%
+      rename(Strata = GP_PRAC_NO, Question = currentQuestion$Question, Weight = currentQuestion$Weight2) %>%
+      mutate(Question = as.character(Question)) %>%
+      filter(Question != 995, Question != 999, Weight != 0) %>%
+      filter(!(is.na(Exclude)==F & Exclude == Question))
+    
+    test <- as_survey(workingWeights, strata = Strata, weight = Weight)
+    test %>% group_by(Question) %>% summarise(pct = survey_prop(deff = "replace"))
+    
+    
 #     %skip2:
 
       
@@ -116,10 +133,24 @@
 #     RUN;
 #     %END;
     
+  
+    currentQuestion <- infoQuestions[[1]]
     
     
     
+    workingWeights <- HACE1920_WEIGHTED %>% 
+      select(GP_PRAC_NO, n_eligible, currentQuestion$Question, currentQuestion$Weight2) %>%
+      mutate(Exclude = currentQuestion$Exclude) %>%
+      rename(Strata = GP_PRAC_NO, Question = currentQuestion$Question, Weight = currentQuestion$Weight2) %>%
+      mutate(Question = as.character(Question)) %>%
+      filter(Question != 995, Question != 999, Weight != 0) %>%
+      filter(!(is.na(Exclude)==F & Exclude == Question))
     
+    test <- as_survey(workingWeights, strata = Strata, weight = Weight)
+    test %>% group_by(Question) %>% summarise(pct = survey_prop(deff = "replace"))
+    
+    
+  
 #     
 #     %IF &QuestionType = Information %THEN %DO;
 #     TITLE "Results: &Question";
